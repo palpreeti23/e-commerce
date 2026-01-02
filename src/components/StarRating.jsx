e77e9565cc262c1339453f0ed5cf3b7238e84072
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-01-02 22:35:22 +0530

    added star rating feature

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaStar, FaRegStar, FaStarHalfAlt } from "react-icons/fa";
+
+function StarRating({ rating, max = 5 }) {
+  return (
+    <div className="flex gap-1 pb-1 px-2 text-left">
+      {[...Array(max)].map((_, index) => {
+        let star = index + 1;
+        if (rating >= star) {
+          return <FaStar key={index} className="text-orange-400 text-sm" />;
+        }
+
+        if (rating >= star - 0.5) {
+          return (
+            <FaStarHalfAlt key={index} className="text-orange-400 text-sm" />
+          );
+        }
+        return <FaRegStar key={index} className="text-gray-600 text-sm" />;
+      })}
+    </div>
+  );
+}
+
+export default StarRating;
