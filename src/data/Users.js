065dc5a57862f3cbdd29ded1c6403a851bfe5b16
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-01-10 23:18:24 +0530

    added mock data

---
+++ src/data/Users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    name: "preeti pal",
+    email: "preeti@gmail.com",
+    password: 12345678,
+  },
+  {
+    name: "guest",
+    email: "guest@gmail.com",
+    password: 11111111,
+  },
+];
+
+export default users;
