f2538ce632a679c8deab05be04c0d524671dd040
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-05-06 12:40:07 +0530

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,80 +3,74 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="w-full h-122 bg-gradient-to-t from-gray-950 to-gray-700 rounded flex justify-center items-center py-5">
-      <div className=" w-[90%] h-auto py-3 bg-gray-800 rounded-lg text-gray-400">
-        <div className="flex flex-col">
-          <div className="w-[18%] h-auto  mt-2">
-            <img
-              className="w-full h-12 object-cover"
-              src="src/img/logo2.png"
-              alt=""
-            />
-          </div>
-          <span className=" border-gray-600 border-b-2 px-5 w-[92%] mx-auto"></span>
-          <p className="text-left pl-15 pb-2 pt-1 text-gray-500 text-sm">
-            Smart deals on Electronics you'll love.
-          </p>
-          <div className="flex justify-around mx-auto  py-2 w-[90%] space-x-2">
-            <div className="flex flex-col text-left px-3 flex-wrap flex-1">
-              <h3 className="text-lg text-gray-300 pb-1 ">About Preetify</h3>
-              <p className="text-gray-500">
-                Built with a focus on simplicity and speed.
-              </p>
+    <div className="w-full h-90 rounded-lg flex justify-center py-5 items-center bg-white flex-wrap text-gray-700 ">
+      <div className="w-[90%] rounded-lg text-gray-700 flex flex-col">
+        <div className="flex justify-around mx-auto space-x-2">
+          <div className="flex flex-col text-left px-3 flex-wrap flex-1">
+            <div className="text-left font-['Saira_Stencil'] font-semibold md:text-xl text-lg">
+              preet<span className="text-orange-500">ify</span>
             </div>
-            <span className="border-r-2 border-gray-600 h-25 w-[1px] "></span>
-            <div className="flex flex-col text-left px-3 flex-wrap flex-1">
-              <h3 className="text-gray-300 pb-1 text-lg">Quick Links</h3>
-              <ul className="flex flex-col space-y-1 list-disc px-5 text-sm">
-                <Link to={`/`}>
-                  <li>Home</li>
-                </Link>
-                <Link to={"/wishlist"}>
-                  <li>Wishlist</li>
-                </Link>
-                <Link to={"/cart"}>
-                  <li>Cart</li>
-                </Link>
-                <Link to={"/orders"}>
-                  <li>Orders</li>
-                </Link>
-              </ul>
-            </div>
-            <span className="border-r-2 border-gray-600 h-25 w-[1px] "></span>
-            <div className="flex flex-col text-left px-3 flex-wrap flex-1 ">
-              <h3 className="text-gray-300 pb-1 text-lg">Customers Support</h3>
-              <ul className="flex flex-col space-y-1 list-disc px-5 text-sm">
-                <li>Help Center</li>
-                <li>Returns</li>
-                <li>Shipping Info</li>
-              </ul>
-            </div>
-            <span className="border-r-2 border-gray-600 h-25 w-[1px] "></span>
-            <div className="flex flex-col text-left px-3 flex-wrap space-y-1 flex-1">
-              <h3 className="text-gray-300 pb-1 text-lg">Follow Us</h3>
-              <ul className="flex space-x-2 ">
-                <li>
-                  <i className="fa-brands fa-linkedin"></i>
-                </li>
-                <li>
-                  <i className="fa-brands fa-github"></i>
-                </li>
-                <li>
-                  <i className="fa-brands fa-x-twitter"></i>
-                </li>
-                <li>
-                  <i className="fa-solid fa-envelope"></i>
-                </li>
-              </ul>
-              <p className="text-sm">Contact@preetify.com</p>
-            </div>
+
+            <p className="text-left pb-2 pt-1 text-gray-800 text-sm w-3/4">
+              Smart deals on Electronics you'll love. Built with a focus on
+              simplicity and speed.
+            </p>
           </div>
 
-          <span className=" border-gray-600 border-b-2 px-5 w-[92%] mx-auto"></span>
-          <p className="pt-1 text-gray-500 text-sm">
-            &copy;2026 Preetify, All rights reserved
-          </p>
+          <div className="flex flex-col text-left md:px-3 flex-wrap flex-1  md:text-xl text-lg">
+            <h3 className="text-gray-900 pb-1 md:text-lg text-md">
+              Quick Links
+            </h3>
+            <ul className="flex flex-col space-y-1 md:list-disc md:px-5 text-sm">
+              <Link to={`/`}>
+                <li>Home</li>
+              </Link>
+              <Link to={"/wishlist"}>
+                <li>Wishlist</li>
+              </Link>
+              <Link to={"/cart"}>
+                <li>Cart</li>
+              </Link>
+              <Link to={"/orders"}>
+                <li>Orders</li>
+              </Link>
+            </ul>
+          </div>
+
+          <div className="flex flex-col text-left md:px-3 flex-wrap flex-1 ">
+            <h3 className="text-gray-900 pb-1 md:text-lg text-md">
+              Customers Support
+            </h3>
+            <ul className="flex flex-col space-y-1 md:list-disc md:px-5 text-sm">
+              <li>Help Center</li>
+              <li>Returns</li>
+              <li>Shipping Info</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col text-left md:px-3 flex-wrap space-y-1 flex-1 px-1">
+            <h3 className="text-gray-900 pb-1 md:text-lg text-md">Follow Us</h3>
+            <ul className="flex flex-col space-x-2 md:flex-row ">
+              <li>
+                <i className="fa-brands fa-linkedin"></i>
+              </li>
+              <li>
+                <i className="fa-brands fa-github"></i>
+              </li>
+              <li>
+                <i className="fa-brands fa-x-twitter"></i>
+              </li>
+              <li>
+                <i className="fa-solid fa-envelope"></i>
+              </li>
+            </ul>
+            <p className="text-sm hidden md:block">Contact@preetify.com</p>
+          </div>
         </div>
+        <span className="block border-gray-200 border-b-2 px-5 w-full mt-4"></span>
+        <p className="pt-1 text-gray-900 text-sm ">
+          &copy;2026 Preetify, All rights reserved
+        </p>
       </div>
     </div>
   );
